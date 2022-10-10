e2ccd75719d18b8d4985618c9d5d9f4bf0134351
Arthur Zimmermann de Oliveira <arthur.zimmermann11@gmail.com>
2022-10-10 20:41:52 -0300

    modificando metodo de login

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, current_app, redirect, url_for, render_template, request, flash
 
 app = Flask(__name__)
@@ -6,44 +5,42 @@ app = Flask(__name__)
 @app.route('/', methods = ['GET','POST'])
 def register():
     if request.method == 'POST':
-        
-        
         name = request.form['nameregister']
         username = request.form['usernameregister']
         password = request.form['passwordregister']
         passwordconfirm = request.form['passwordregisterconfirm']
         email = request.form['emailregister']
-        dictLog = {
-            "name" : name,
-            "username": username,
-            "password" : password,
-            "email" : email,
-        }
-        print(username)
-        print(password)
-        print(dictLog)
         if password != passwordconfirm:
             return render_template('error.html')
         else: 
-            return render_template('login.html')
+            dictLog = {
+                "name" : name,
+                "username": username,
+                "password" : password,
+                "email" : email,
+            }
+            print(username)
+            print(password)
+            print(dictLog)
+            return redirect('/login')
     return render_template('registerpage.html')
 
 
 
 @app.route('/login' ,methods = ['GET','POST'])  
-def login(self):      
+def login():      
     error = None
     
     if request.method == 'POST': 
-        for v in self.dictLog.values():
-            if self.dictLog['username'] == request.form['username'] and self.dictLog['password'] == request.form['username']: 
-                return render_template('firstpage.html')
-            else: 
-                error = 'Invalid username or password. Please try again !'
-    return render_template('login.html', error = error) 
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin': 
+            return redirect('/home')
+        else: 
+            error = 'Invalid username or password. Please try again !'
+    return render_template('login.html', error = error)
 
-    #LOGIN PADRÃO : admin
-    #SENHA PADRÃO : admin
+@app.route('/home', methods = ['GET','POST'])
+def home():
+    return render_template('firstpage.html')
 
 if __name__ == "__main__":
     app.run()
